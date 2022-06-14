b12ad245f1e31c3a1de95354b807ee279cc8d4d0
Rochambeau WITTA <wittarochambeau@gmail.com>
2022-06-14 17:05:45 +0000

    changes

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,7 @@
 //* my main.js
+function tva(){
+    let prixHT = Number(document.getElementById("ht").value);
+    let prixTva = prixHT*0.2;
+    let prixTTC = prixHT+prixTva;
+    document.getElementById("prix").innerHTML = "Votre prix avec tva est de " + prixTTC + " FCFA";
+}
